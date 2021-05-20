0148fae7ad8fb637679203a959911b0baaef8f04
sousouben <s.mouatakide@laposte.net>
2021-05-20 11:42:15 +0200

    installation muler

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');// Importation d'express
 const bodyParser = require('body-parser');//Pour gérer la demande POST provenant de l'application front-end, nous devrons être capables d'extraire l'objet JSON de la demande. 
 const mongoose = require('mongoose');
+const path = require('path');
 
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 // Transforme les données arrivant de la requête POST en un objet JSON facilement exploitable
 app.use(bodyParser.json());
 
+app.use('/images',express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
--- Backend/routes/sauces.js
+++ Backend/routes/sauces.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-
+const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', auth, sauceCtrl.createSauce);  
-router.put('/:id', auth, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/ ', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce); 
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;

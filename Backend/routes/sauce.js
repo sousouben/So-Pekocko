d9aa253f94a2cd3964978f22c46b36157472a099
sousou ben <s.mouatakide@laposte.net>
2021-05-26 19:19:04 +0200

    modif espace get routers

--- Backend/routes/sauce.js
+++ Backend/routes/sauce.js
@@ -6,11 +6,11 @@ const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 //ordre important: auth avant multer sinon image de requete non identifie peuvent etre enregistre
-router.get('/ ', auth, sauceCtrl.getAllSauces);//récupère des infos
+router.get('/', auth, sauceCtrl.getAllSauces);//récupère des infos
 router.post('/', auth, multer, sauceCtrl.createSauce); //donne des infos
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);//remplace une info
-router.delete('/:id', auth, sauceCtrl.deleteSauce);//supprimer une ressource
+router.delete('/:id', auth, sauceCtrl.deleteSauce);//supprimer une sauce
 router.post('/:id/like', auth, sauceCtrl.likeOrDislike);
 
 module.exports = router;

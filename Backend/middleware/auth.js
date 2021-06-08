431d9bbab6545f1a2e058df00fa754a099dec967
sousou ben <s.mouatakide@laposte.net>
2021-06-08 21:26:59 +0200

    modif controllers

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');//Création d'un token pour gérer l'authent
 
 require('dotenv').config()
 
-module.exports = (req, res, next) => {//Ce middleware permettra de protéger les routes sélectionnées et permettra de vérifier que l'utilisateur est authentifié avant d'autoriser l'envoi de requêtes
+module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];//Extraction du token du header Authorization de la requête entrante. Il contient le mot-clé Bearer . Utilisation de la fonction split pour récupérer tout après l'espace dans le header. Les erreurs générées ici s'afficheront dans le bloc catch ;
-    const decodedToken = jwt.verify(token, process.env.TOKEN);//utilisation de la fonction verify pour décoder notre token. Si celui-ci n'est pas valide, une erreur sera générée
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN);
     const userId = decodedToken.userId;//extraction de l'ID utilisateur de notre token 
-    if (req.body.userId && req.body.userId !== userId) {//si la demande contient un ID utilisateur, nous le comparons à celui extrait du token. S'ils sont différents, nous générons une erreur sinon  l‘utilisateur est authentifié. Nous passons l'exécution à l'aide de la fonction next()
+    if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
       next();
--- Backend/middleware/multer-config.js
+++ Backend/middleware/multer-config.js
@@ -6,15 +6,15 @@ const MIME_TYPES = {//Dictionnaire des différents types d'images
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({//Constante storage à passer à multer comme configuration, qui contient la logique nécessaire pour indiquer à multer où enregistrer les fichiers entrants
-  destination: (req, file, callback) => {//la fonction destination indique à multer d'enregistrer les fichiers dans le dossier images
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
     callback(null, 'images');
   },
-  filename: (req, file, callback) => {//La fonction filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier. Elle utilise ensuite la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée
-    const name = file.originalname.split(' ').join('_');
+  filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
 });
 
-module.exports = multer({ storage }).single('image');//Export multer entièrement configuré, lui passons notre constante storage et lui indiquons que nous gérerons uniquement les téléchargements de fichiers image.
+module.exports = multer({ storage : storage }).single('image');
